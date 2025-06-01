1b2bdc2c6e447fde5485bfc903f2d241a6201ac0
zq6966 <zq6966@partner.kit.edu>
2025-06-01 12:15:36 +0200

    SelectionFunctions.kt documentation

--- src/main/kotlin/discreteChoice/selection/SelectionFunctions.kt
+++ src/main/kotlin/discreteChoice/selection/SelectionFunctions.kt
@@ -4,10 +4,17 @@ import discreteChoice.SelectionFunction
 import discreteChoice.utility.select
 import kotlin.random.Random
 
+/**
+ * A SelectionFunction, which selects one of the options based on their assigned probability.
+ */
 class RandomWeightedSelect<X> : SelectionFunction<X> {
     override fun calculateSelection(options: Map<X, Double>, random: Random): X = options.select(random.nextDouble())
 }
 
+/**
+ * A SelectionFunction, which selects one of the options without regarding the assigned probabilities. Acts like every
+ * option has the same probability.
+ */
 class UniformSelect<X> : SelectionFunction<X> {
     override fun calculateSelection(options: Map<X, Double>, random: Random): X {
         val index = random.nextInt(options.size)
