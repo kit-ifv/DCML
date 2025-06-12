bc75b85d653aadf54b3841e0ce1ade0f3b02e5f7
zq6966 <zq6966@partner.kit.edu>
2025-06-12 23:16:09 +0200

    added dokka for generating docs

--- src/main/kotlin/discreteChoice/distribution/MultinomialLogit.kt
+++ src/main/kotlin/discreteChoice/distribution/MultinomialLogit.kt
@@ -3,6 +3,12 @@ package discreteChoice.distribution
 import discreteChoice.DistributionFunction
 import kotlin.math.exp
 
+/**
+ * Classic soft-max approach to calculating probabilities out of utility-values.
+ * Probability of option p_i := e^u_i / {sum of all utilities  e^u_k}
+ * TODO test what happens when u_i > 200 or something so when e^u_i is practicly infinity.
+ * Or define how it's supposed to work.
+ */
 class MultinomialLogit<X, P> : DistributionFunction<X, P> {
 
     override fun calculateProbabilities(utilities: Map<X, Double>, parameters: P): Map<X, Double> {
