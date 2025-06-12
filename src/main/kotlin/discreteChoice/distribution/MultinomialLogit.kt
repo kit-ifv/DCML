abfb85eee4bf2dc8c0faf7cb9a06f03308fc1515
zq6966 <zq6966@partner.kit.edu>
2025-06-13 00:18:59 +0200

    added documentation, tests and implementation of MultinomialLogits edge cases

--- src/main/kotlin/discreteChoice/distribution/MultinomialLogit.kt
+++ src/main/kotlin/discreteChoice/distribution/MultinomialLogit.kt
@@ -6,8 +6,14 @@ import kotlin.math.exp
 /**
  * Classic soft-max approach to calculating probabilities out of utility-values.
  * Probability of option p_i := e^u_i / {sum of all utilities  e^u_k}
- * TODO test what happens when u_i > 200 or something so when e^u_i is practicly infinity.
- * Or define how it's supposed to work.
+ *
+ * __Edge cases:__
+ *
+ * If utilities greater than 710 exist, each option with a utility >= 710 gets an equal probability, all others get 0. (
+ * because `exp(710) == Double.POSITIVE_INFINITY`)
+ *
+ * If utilities are all  <=-745 each option gets the same probability. (Because exp(-745) == 0.0 leading division with
+ * zero)
  */
 class MultinomialLogit<X, P> : DistributionFunction<X, P> {
 
@@ -15,8 +21,18 @@ class MultinomialLogit<X, P> : DistributionFunction<X, P> {
         val currentExp = utilities.entries.associate {
             it.key to exp(it.value)
         }
+        val countInfinity = currentExp.count { it.value == Double.POSITIVE_INFINITY }
+        if (countInfinity > 0) {
+            val equalPos = currentExp.mapValues {
+                if (it.value == Double.POSITIVE_INFINITY) 1.0 / countInfinity else 0.0
+            }
+            return equalPos
+        }
         val sum = currentExp.values.sum()
 
+        if(sum == 0.0) {
+            return utilities.mapValues { 1.0 / utilities.size }
+        }
         return currentExp.mapValues { it.value / sum }
     }
 }
