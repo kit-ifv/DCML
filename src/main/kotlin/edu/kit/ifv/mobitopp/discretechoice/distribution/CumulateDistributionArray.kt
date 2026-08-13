f2f75f7375148cd7532afb66e2483b8947635b63
Anton <ueida@student.kit.edu>
2026-08-13 14:29:29 +0200

    doc for FloatCumulateDistributionArray

--- src/main/kotlin/edu/kit/ifv/mobitopp/discretechoice/distribution/CumulateDistributionArray.kt
+++ src/main/kotlin/edu/kit/ifv/mobitopp/discretechoice/distribution/CumulateDistributionArray.kt
@@ -11,10 +11,19 @@ package edu.kit.ifv.mobitopp.discretechoice.distribution
  * values underflow to zero. In that case, the caller is expected to use a fallback strategy.
  */
 fun interface CumulateDistributionArray<in P> {
+    /**
+     * @return `true` if a valid cumulative distribution was written into the given [utilities] array. Returns `false` if the
+     *  * utilities cannot produce positive finite probability mass, for example because all relevant
+     *  * values underflow to zero. In that case, the caller is expected to use a fallback strategy.
+     */
     fun tryCumulateProbabilities(utilities: DoubleArray, parameters: P): Boolean
 }
 
-
+/**
+ * Note: this extension function first calculates the cumulated probabilities and then "de-cumulates" them,
+ * so might be worth optimizing, if this is in a hot section.
+ * @return the probabilities in a __non-cumulated__ form.
+ */
 fun <P> CumulateDistributionArray<P>.probabilities(utilities: DoubleArray, parameters: P): DoubleArray {
     val success = tryCumulateProbabilities(utilities, parameters)
     if(!success) { throw IllegalStateException("Cannot generate probabilities due to error in calculation, the result" +
